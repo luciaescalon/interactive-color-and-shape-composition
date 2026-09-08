42503a4d2010213334edc60e856c0dac55d85054
luciaescalon <escalonlucia@gmail.com>
2026-09-08 11:01:19 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,61 @@
-let bgColor;
-let circleColor;
-let flower;
+let happyBool = false;
+let sadBool = false;
 
-async function setup() {
-  createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER);
-
-  flower = await loadImage("flower.png");
-
-  bgColor = color(222, 44, 172);
-  circleColor = color(130, 200, 75);
+function setup() {
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(bgColor);
+  background(244, 241, 234);
 
-  stroke(120, 40, 200);
-  strokeWeight(10);
-  line(50, 100, 500, 600);
+  if (happyBool === true) {
+    happyFace();
+  }
+  if (sadBool === true) {
+    sadFace();
+  }
+}
 
-  // this is how you write notes
+function happyFace() {
+  fill(255, 221, 89);
+  stroke(0);
+  strokeWeight(3);
+  circle(200, 200, 240);
 
-  fill(circleColor);
-  stroke(70, 140, 100);
-  strokeWeight(5);
-  ellipse(300, 350, 100, 100);
-
-  // notes
-  fill(80, 120, 220);
-  rect(450, 200, 100, 200);
-
-  fill(230, 200, 80, 200);
+  fill(0);
   noStroke();
-  ellipse(400, 200, 50, 50);
+  circle(150, 170, 20);
+  circle(250, 170, 20);
 
   noFill();
-  stroke(70, 200, 100);
-  strokeWeight(20);
-  triangle(100, 500, 200, 550, 50, 600);
-
-  // IMAGE
-  image(flower, 400, 450, 100, 100);
+  stroke(0);
+  strokeWeight(6);
+  arc(200, 200, 120, 100, 0.2, PI - 0.2);
 }
 
+function sadFace() {
+  fill(120, 180, 255);
+  stroke(0);
+  strokeWeight(3);
+  circle(200, 200, 240);
 
-// MOUSE EVENT
+  fill(0);
+  noStroke();
+  circle(150, 170, 20);
+  circle(250, 170, 20);
+
+  noFill();
+  stroke(0);
+  strokeWeight(6);
+  arc(200, 230, 120, 100, PI + 0.2, TWO_PI - 0.2);
+}
+
 function mousePressed() {
-  bgColor = color(
-    random(255),
-    random(255),
-    random(255)
-  );
+  happyBool = true;
+  sadBool = false;
 }
 
-
-// KEY PRESSED EVENT
 function keyPressed() {
-  circleColor = color(
-    random(255),
-    random(255),
-    random(255)
-
-    
-  );
+  sadBool = true;
+  happyBool = false;
 }
